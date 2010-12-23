494b88e61144706fc4e49eba29c72ddad1b89b20
ArcterusEmu <arcterusemu@web.de>
2010-12-23 18:20:53 +0100

    Database patches for Revision 4

---
+++ files/patches/world/9_world_command.sql
@@ -0,0 +1,18 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : ActerusEmu
+Source Server Version : 50141
+Source Host           : localhost:3306
+Source Database       : world
+
+Target Server Type    : MYSQL
+Target Server Version : 50141
+File Encoding         : 65001
+
+Date: 2010-12-23 18:16:19
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+INSERT INTO `command` VALUES ('account set closed', '3', 'Syntax: .account set closed $account $time $reason | bans an account');
